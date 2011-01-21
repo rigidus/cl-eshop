4db2a0ab96ec7f894ae99c19c332b26438ce932f
Rigidus <avenger-f@yandex.ru>
2011-01-21 22:12:44 +0300

    bug

--- gateway.lisp
+++ gateway.lisp
@@ -1,10 +1,5 @@
 (in-package #:gateway)
 
-(defclass gateway ()
-  ((timepoint        :initarg :time              :initform nil       :accessor   timepoint)
-   (raw              :initarg :raw               :initform nil       :accessor   raw)
-   (data             :initarg :data              :initform nil       :accessor   data)))
-
 
 (funcall *dispatcher*
          `((string= "/gateway" (service:request-str))
@@ -20,76 +15,62 @@
                                               :if-exists :supersede
                                               :external-format :utf-8)
                           (format file "~a" raw))
-                        ;; Создаем объект
-                        (let ((gateway (make-instance 'gateway :raw raw)))
-                          ;; Обрабатываем объект
-                          (process gateway))
-                        "OK"
+                        ;; Обработка
+                        (format nil "~a" (process raw))
                         ))))))
 
 
-;; Представление объекта в консоли
-(defmethod plist-representation ((object gateway) &rest fields)
-  (let ((result))
-    (loop :for item :in fields do
-       (let ((method (intern (symbol-name item) 'gateway)))
-         (push item result)
-         (push (funcall method object) result)))
-    (reverse result)))
-
-
-(defmethod process ((object gateway) &key)
-  ;; Устанавливаем метку времени
-  (setf (timepoint object) (get-date-time))
+(defun process (raw)
   ;; Декодируем JSON из RAW в DATA
-  (setf (data object)
-        (json:decode-json-from-string
-         (sb-ext:octets-to-string (raw object) :external-format :cp1251)))
-  ;; Делаем все продукты неактивными
-  (maphash #'(lambda (k v)
-               (setf (product:active v ) nil))
-           trans:*product*)
-  ;; Перебираем продукты
-  (loop
-     :for elt
-     :in  (data object) do
-     (block iteration
-       (let ((articul   (ceiling (parse-float (cdr (assoc :id elt)))))
-             (price     (ceiling (parse-float (cdr (assoc :price elt)))))
-             (isnew     (cdr (assoc :isnew  elt)))
-             (isspec    (cdr (assoc :isspec elt)))
-             (count-total    (ceiling (parse-float (cdr (assoc :count--total elt)))))
-             (count-transit  (ceiling (parse-float (cdr (assoc :count--transit elt))))))
-         ;; Нам не нужны продукты с нулевой ценой (вероятно это группы продуктов)
-         (when (equal 0 price)
-           ;; (print elt)
-           (return-from iteration))
-         ;; Проверяем, есть ли у нас уже этот продукт
-         (if (null (gethash articul trans:*product*))
-             ;; Это новый продукт
-             (setf (gethash articul trans:*product*)
-                   (make-instance 'product:product
-                                  :articul        articul
-                                  :name           (cdr (assoc :name elt))
-                                  :price          price
-                                  :siteprice      (ceiling (parse-float (cdr (assoc :price--site elt))))
-                                  :ekkprice       (ceiling (parse-float (cdr (assoc :price--ekk elt))))
-                                  :active         (if (= count-total 0 ) nil t)
-                                  :newbie	      (if (string= "0" isnew) nil t)
-                                  :sale           (if (string= "0" isspec) nil t)
-                                  :count-total    count-total
-                                  :count-transit  count-transit
-                                  ))
-             ;; else Это уже существующий продукт
-             (let ((product (gethash articul trans:*product*)))
-               (setf (product:name  product)          (cdr (assoc :name elt)))
-               (setf (product:price product)          price)
-               (setf (product:siteprice product)      (ceiling (parse-float (cdr (assoc :price--site elt)))))
-               (setf (product:ekkprice product)       (ceiling (parse-float (cdr (assoc :price--ekk elt)))))
-               (setf (product:active product)         (if (= count-total 0 ) nil t))
-               (setf (product:newbie product)         (if (string= "0" isnew) nil t))
-               (setf (product:sale product)           (if (string= "0" isspec) nil t))
-               :count-total    count-total
-               :count-transit  count-transit
-               )
-             )))))
+  (let ((data (json:decode-json-from-string
+               (sb-ext:octets-to-string raw :external-format :cp1251))))
+    ;; dbg
+    ;; (format nil "~a" data)
+
+    ;; Делаем все продукты неактивными
+    (maphash #'(lambda (k v)
+                 (setf (product:active v ) nil))
+             trans:*product*)
+
+    ;; Перебираем продукты
+    (loop :for elt  :in data :collect
+       (block iteration
+         (let ((articul   (ceiling (parse-float (cdr (assoc :id elt)))))
+               (price     (ceiling (parse-float (cdr (assoc :price elt)))))
+               (siteprice (ceiling (parse-float (cdr (assoc :price--site elt)))))
+               (ekkprice  (ceiling (parse-float (cdr (assoc :price--ekk elt)))))
+               (isnew     (cdr (assoc :isnew  elt)))
+               (isspec    (cdr (assoc :isspec elt)))
+               (name      (cdr (assoc :name elt)))
+               (realname  (cdr (assoc :realname elt)))
+               (count-total    (ceiling (parse-float (cdr (assoc :count--total elt)))))
+               (count-transit  (ceiling (parse-float (cdr (assoc :count--transit elt))))))
+           ;; Нам не нужны продукты с нулевой ценой (вероятно это группы продуктов)
+           (when (equal 0 price)
+             (return-from iteration))
+           (process-product articul price siteprice ekkprice isnew isspec name realname count-total count-transit))))))
+
+
+(defun process-product (articul price siteprice ekkprice isnew isspec name realname count-total count-transit)
+  (let ((product (aif (gethash articul trans:*product*)
+                      it
+                      (make-instance 'product:product :articul articul))))
+    (setf (product:articul product)         articul
+          (product:name product)            name
+          (product:realname product)        (if (or (null realname)
+                                                    (string= "" realname))
+                                                name
+                                                realname)
+          (product:price product)           price
+          (product:siteprice product)       siteprice
+          (product:ekkprice product)        ekkprice
+          (product:active product)          t ;;(if (= count-total 0 ) nil t)
+          (product:newbie product)	        (if (string= "0" isnew) nil t)
+          (product:sale product)            (if (string= "0" isspec) nil t)
+          (product:count-total product)     count-total
+          (product:count-transit  product)  count-transit)
+    (setf (gethash articul trans:*product*) product)
+    (if (= articul 147421)
+        (product:plist-representation product :articul :name :realname :count-total :active)
+        "")))
+
