da5403e998cb2730a246a30827da62c1d0cb322d
Wolfor <wolforus@gmail.com>
2011-01-25 18:38:01 +0300

    yml change logic

--- yml.lisp
+++ yml.lisp
@@ -1,6 +1,19 @@
 (in-package #:yml)
 
-(funcall cl-user:*dispatcher*
+;; Группы представляют собой лес для YML нам нужны не только сами
+;; группы маркированные для выгрузки но и их родители
+;; На самом деле нам нужно минимальные оставные деревья,
+;; но будем выгружать полные деревья исключая только листья, если это нужно
+(defun yml-groups ()
+(let ((groups))
+  (maphash #'(lambda(k g) (if (or
+                               (not (null (group:childs g)))
+                               (group:ymlshow g))
+                            (setf groups (cons g groups)))) trans:*group*)
+  groups))
+
+
+(funcall cl-eshop:*dispatcher*
          `((string= "/yml" (service:request-str))
            ,#'(lambda ()
                 (setf (hunchentoot:content-type*) "application/xml; charset=utf-8")
@@ -9,31 +22,28 @@
                                :marketcompany "ЦиFры 320-8080"
                                :marketurl "http://www.320-8080.ru/"
                                :categoryes
-                               (append
-                                (list
-                                 (list :id 9999999
-                                       :name "root"
-                                       :parent 0))
-                                (loop
-                                   :for group
-                                   :being :the hash-values
-                                   :in trans:*group*
-                                   :when (and (equal (type-of group) 'group:group)
-                                              (group:ymlshow group))
-                                   :collect (list :id (group:id group)
-                                                  :name (group:name group)
-                                                  :parent (if (null (group:parent group))
-                                                              9999999
-                                                              (group:id (group:parent group))))))
+                               (loop
+                                  :for g
+                                  :in (yml-groups)
+                                  :collect (list :id (group:id g)
+                                                 :name (group:name g)
+                                                 :parent (if (null (group:parent g))
+                                                             -1 ;; если это вершина дерева
+                                                             (group:id (group:parent g)))))
+
 
                                :offers (format nil "~{~a~}"
                                (loop
-                                  ;; :repeat 50
                                   :for product
                                   :being :the hash-values
                                   :in trans:*product*
-                                  :when (and (equal (type-of (product:parent product)) 'group:group)
-                                             (group:ymlshow (product:parent product)))
+                                  ;;продукт должен находиться в группе маркированной как ymlshow
+                                  ;;быть активным и иметь не нулевую цену
+                                  :when (and (not (null (product:parent product)))
+                                             (group:ymlshow (product:parent product))
+                                             (product:active product)
+                                             (not (null (product:price product)))
+                                             (> (product:price product) 0))
                                   :collect (yml:offer (list :articul (product:articul product)
                                                             :price (product:price product)
                                                             :category (group:id (product:parent product))
