c1272ce568640aa30b10631e1222dbcfe22150a2
Rigidus <avenger-f@yandex.ru>
2011-02-03 18:05:42 +0300

    restore wolfor-stuff conflict

--- wolfor-stuff.lisp
+++ wolfor-stuff.lisp
@@ -4,15 +4,15 @@
   (let ((value-f (getf request-get-plist (read-from-string (format nil ":~a-f" opt-label))))
         (value-t (getf request-get-plist (read-from-string (format nil ":~a-t" opt-label))))
         (product-value 0))
-    (mapcar #'(lambda (optgroup)
-                (if (string= (eshop::name optgroup) opt-group-name)
-                    (let ((options (eshop::options optgroup)))
+    (mapcar #'(lambda (option)
+                (if (string= (optgroup::name option) opt-group-name)
+                    (let ((options (optgroup::options option)))
                       (mapcar #'(lambda (opt)
-                                  (if (string= (eshop::name opt) opt-name)
+                                  (if (string= (option::name opt) opt-name)
                                       (setf product-value
-                                            (format nil "~a" (eshop::value opt)))))
+                                            (format nil "~a" (option::value opt)))))
                               options))))
-            (eshop::optgroups product))
+            (optlist:optlist (product:options product)))
     (if (null product-value)
         (setf product-value "0"))
     (if (null value-f)
@@ -32,107 +32,72 @@
 (defun checkbox-filter (p g) t)
 
 (defun price-filter (product request-get-plist)
-  (let ((price-f
-         (parse-integer
-          (getf request-get-plist :price-f)
-          :junk-allowed t))
-        (price-t
-         (parse-integer
-          (getf request-get-plist :price-t)
-          :junk-allowed t))
-        (product-price
-         (eshop::price product)))
-    (unless (integerp price-f)
-      (setf price-f 0))
-    (unless (integerp price-t)
-      (setf price-t 999999999999))
-    (print (list price-f price-t))
-    (and
-     (<= price-f product-price)
-     (>= price-t
-         product-price))))
+  (LET ((PRICE-F
+         (PARSE-INTEGER
+          (GETF REQUEST-GET-PLIST :PRICE-F)
+          :JUNK-ALLOWED T))
+        (PRICE-T
+         (PARSE-INTEGER
+          (GETF REQUEST-GET-PLIST :PRICE-T)
+          :JUNK-ALLOWED T))
+        (PRODUCT-PRICE
+         (PRODUCT:PRICE PRODUCT)))
+    (UNLESS (INTEGERP PRICE-F)
+      (SETF PRICE-F 0))
+    (UNLESS (INTEGERP PRICE-T)
+      (SETF PRICE-T 999999999999))
+    (PRINT (LIST PRICE-F PRICE-T))
+    (AND
+     (<= PRICE-F PRODUCT-PRICE)
+     (>= PRICE-T
+         PRODUCT-PRICE))))
 
 (defun radio-filter (p g opts og o) t)
 
 
 (defun num-active-product (products)
   (let ((num 0))
-<<<<<<< HEAD
-    (mapcar #'(lambda (p) (if (active p) (incf num))) products)
-=======
     (mapcar #'(lambda (p)
                 (if (product:active p)
                     (incf num)))
             products)
->>>>>>> wolfor-master
     num))
 
 (defun sum-prices-product (products)
   (let ((sum 0))
-<<<<<<< HEAD
-    (mapcar #'(lambda (p) (if (active p) (setf sum (+ (price p) sum)))) products)
-=======
     (mapcar #'(lambda (p)
                 (if (product:active p)
                     (setf sum (+
                                (product:price p)
                                sum))))
             products)
->>>>>>> wolfor-master
     sum))
 
 (defun get-products-list ()
   (let ((product-list))
-<<<<<<< HEAD
-    (maphash #'(lambda (k v) (setf product-list (cons v product-list ))) eshop::*storage* )
-=======
     (maphash #'(lambda (k v)
                  (setf product-list (cons v product-list )))
              trans:*product* )
->>>>>>> wolfor-master
     product-list))
 
 (defun get-group-routs ()
   (let ((routs))
     (maphash #'(lambda (k g)
-<<<<<<< HEAD
-                 (if (null (parent g))
-                     (setf routs (cons g routs))))
-             eshop::*storage*)
-=======
                  (if (null (group:parent g))
                      (setf routs (cons g routs))))
              trans:*group*)
->>>>>>> wolfor-master
     routs))
 
 (defun get-group-active ()
   (let ((routs))
-<<<<<<< HEAD
-    (maphash #'(lambda (k g) (if (active g) (setf routs (cons g routs)))) eshop::*storage*)
-=======
     (maphash #'(lambda (k g)
                  (if (group:active g)
                      (setf routs (cons g routs))))
              trans:*group*)
->>>>>>> wolfor-master
     routs))
 
 (defun get-group-ymlshow ()
   (let ((routs))
-<<<<<<< HEAD
-    (maphash #'(lambda (k g) (if (ymlshow g) (setf routs (cons g routs)))) eshop::*storage*)
-    routs))
-
-(defun show-group (g n)
-  (format t "~&~Va~a: ~a  | yml:~a active:~a  продуктов:~a  (key:~a)" n "" (id g) (name g) (ymlshow g) (active g) (length (get-recursive-products g)) (key g))
-  (mapcar #'(lambda (g) (show-group g (+ n 5))) (childs g))
-  )
-
-(defun num-product-in-groups (groups)
-  (let ((num 0))
-    (mapcar #'(lambda (g) (setf num (+ num (length (remove-if #'(lambda(g) (not (active g))) (products g)))))) groups)
-=======
     (maphash #'(lambda (k g)
                  (if (group:ymlshow g)
                      (setf routs (cons g routs))))
@@ -161,15 +126,14 @@
                                       #'(lambda(g) (not (group:active g)))
                                       (group:products g))))))
             groups)
->>>>>>> wolfor-master
     num))
 
 
 
 (defun num-active-product-with-bad-price ()
-  (length (remove-if #'(lambda(p) (or (not (active p))
-                                      (and (not (null (price p)))
-                                           (> (price p) 0)))) (get-products-list))))
+  (length (remove-if #'(lambda(p) (or (not (product:active p))
+                                      (and (not (null (product:price p)))
+                                           (> (product:price p) 0)))) (get-products-list))))
 
 (defun statistics ()
   (mapcar #'(lambda (g) (show-group g 0)) (get-group-routs))
@@ -181,11 +145,7 @@
                                                (/ (sum-prices-product (get-products-list))
                                                   n)
                                                0)))
-<<<<<<< HEAD
-  (format t "~&Количество групп: ~a" (hash-table-count eshop::*storage*))
-=======
   (format t "~&Количество групп: ~a" (hash-table-count trans:*group*))
->>>>>>> wolfor-master
   (format t "~&Количество активных групп: ~a" (length (get-group-active)))
   (format t "~&Количество YML групп: ~a" (length (get-group-ymlshow)))
   (format t "~&Количество активных товаров с нулевой ценой: ~a" (num-active-product-with-bad-price))
@@ -193,31 +153,30 @@
   )
 
 
-<<<<<<< HEAD
 ;; (let ((num 0))
-;;   (maphash #'(lambda (k product)
-;;                (if (and (not (null (product:parent product)))
-;;                         (group:ymlshow (product:parent product))
-;;                         (product:active product)
-;;                         (not (null (product:price product)))
-;;                         (> (product:price product) 0))
-;;                    (incf num)))
-;;            trans:*product*)
-;;   num)
+;;   (maphash
+;;    #'(lambda (k product)
+;;        (if (and (not (null (product:parent product)))
+;;                 (group:ymlshow (product:parent product))
+;;                 (product:active product)
+;;                 (not (null (product:price product)))
+;;                 (> (product:price product) 0))
+;;            (incf num)))
+;;    trans:*product*) num)
 
 
 (defun num-products-in-group (g)
   (let ((cnt 0))
     (maphash #'(lambda (k v)
-                 (if (equal g (parent v))
+                 (if (equal g (product:parent v))
                      (incf cnt)))
-             eshop::*storage*)
+             trans:*product*)
     cnt))
 
 ;; (maphash #'(lambda (k v)
 ;;              (if (not (= (num-products-in-group v)
 ;;                          (length (group:products v))))
-;;                  (format t "~&~a ~a:~a"
+;;                  (format t "~&~a  ~a:~a"
 ;;                          (group:key v)
 ;;                          (num-products-in-group v)
 ;;                          (length (group:products v)))))
@@ -227,39 +186,3 @@
 ;;  (mapcar #'(lambda (v)
 ;;              (format nil "> ~a" (product:parent v)))
 ;;          (group:products (gethash "pamyat-dlya-noutbukov" trans:*group*))))
-
-=======
-  ;; (let ((num 0))
-  ;;   (maphash
-  ;;    #'(lambda (k product)
-  ;;        (if (and (not (null (product:parent product)))
-  ;;                 (group:ymlshow (product:parent product))
-  ;;                 (product:active product)
-  ;;                 (not (null (product:price product)))
-  ;;                 (> (product:price product) 0))
-  ;;            (incf num)))
-  ;;    trans:*product*) num)
-
-
-  (defun num-products-in-group (g)
-    (let ((cnt 0))
-      (maphash #'(lambda (k v)
-                   (if (equal g (product:parent v))
-                       (incf cnt)))
-               trans:*product*)
-      cnt))
-
-  ;; (maphash #'(lambda (k v)
-  ;;              (if (not (= (num-products-in-group v)
-  ;;                          (length (group:products v))))
-  ;;                  (format t "~&~a  ~a:~a"
-  ;;                          (group:key v)
-  ;;                          (num-products-in-group v)
-  ;;                          (length (group:products v)))))
-  ;;          trans:*group*)
-
-  ;; (length
-  ;;  (mapcar #'(lambda (v)
-  ;;              (format nil "> ~a" (product:parent v)))
-  ;;          (group:products (gethash "pamyat-dlya-noutbukov" trans:*group*))))
->>>>>>> wolfor-master
