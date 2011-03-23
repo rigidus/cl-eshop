25dd37029ebbdc3f5f54a4a1fc4ac2ed7e99bef5
Wolfor <wolforus@gmail.com>
2011-03-23 15:10:22 +0300

    emacs startup script

---
+++ start.el
@@ -0,0 +1,24 @@
+(mapc #'(lambda (file)
+           (find-file file)
+           (slime-compile-region 1 (length (buffer-string)))
+           (read-char)
+           (kill-buffer (current-buffer))
+           )
+       '(
+         "~/cl-eshop/start.lisp"
+         "~/cl-eshop/eshop-config.lisp"
+         "~/cl-eshop/errors.lisp"
+         "~/cl-eshop/spike.lisp"
+         "~/cl-eshop/classes.lisp"
+         "~/cl-eshop/serializers.lisp"
+         "~/cl-eshop/servo.lisp"
+         "~/cl-eshop/trans.lisp"
+         "~/cl-eshop/routes.lisp"
+         "~/cl-eshop/render.lisp"
+         "~/cl-eshop/cart.lisp"
+         "~/cl-eshop/generics.lisp"
+         "~/cl-eshop/gateway.lisp"
+         "~/cl-eshop/search.lisp"
+         "~/cl-eshop/xls.lisp"
+         "~/cl-eshop/yml.lisp"
+         ))
