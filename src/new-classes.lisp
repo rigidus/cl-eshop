9cf89afd54a36febd553acaf4c20ed6b54f0b44d
Wolfor <wolforus@gmail.com>
2011-10-10 18:50:48 +0400

    mrg

--- src/new-classes.lisp
+++ src/new-classes.lisp
@@ -1,88 +1,31 @@
 (in-package #:eshop)
 
-
-(defun new-classes.view-string-field (value name disabled)
-  (soy.class_forms:string-field
-   (list :name name :disabled disabled :value value)))
-
-(defun new-classes.string-field-get-data (string)
-  string)
-
-(defun new-classes.view-int-field (value name disabled)
-  (new-classes.view-string-field (format nil "~a" value) name disabled))
-
-(defun new-classes.int-field-get-data (string)
-  (parse-integer string))
-
-(defun new-classes.view-textedit-field (value name disabled)
-  (if disabled
-      (new-classes.view-string-field value name disabled)
-      (soy.class_forms:texteditor
-       (list :name name :value value))))
-
-(defun new-classes.textedit-field-get-data (string)
-  string)
-
-
-(defun new-classes.view-time-field (value name disabled)
-  (new-classes.view-string-field (time.decode-date-time value) name disabled))
-
-(defun new-classes.time-field-get-data (string)
-  string)
-
-(defun new-classes.view-bool-field (value name disabled)
-  (soy.class_forms:bool-field
-   (list :name name :checked value :disabled disabled)))
-
-
-(defun new-classes.bool-field-get-data (string)
-  (string= string "T"))
-
-(defun new-classes.view-group-field (value name disabled)
-  (let ((leveled-groups (storage.get-groups-leveled-tree)))
-    (soy.class_forms:group-form
-     (list :name name :disabled disabled
-           :grouplist (mapcar #'(lambda (group-and-level)
-                                  (let ((group (car group-and-level))
-                                        (level (cdr group-and-level)))
-                                    (list :hashkey (key group)
-                                          :selected (eq value group)
-                                          :name (name group)
-                                          :indent (let ((indent ""))
-                                                    (loop for x from 1 to level
-                                                       do (setf indent (concatenate 'string indent "---")))
-                                                    indent))))
-                              leveled-groups)))))
-
-(defun new-classes.group-field-get-data (string)
-  (gethash string (storage *global-storage*)))
-
-
-
-
-
-
+;;макрос для создания класса по списку параметров
 (defmacro new-classes.make-class (name class-fields)
   `(defclass ,name ()
      ,(mapcar #'(lambda (field)
                   `(,(getf field :name)
-                     :initarg ,(getf field :initarg)
+                     :initarg ,(intern (format nil "~a" (getf field :name)) :keyword)
                      :initform ,(getf field :initform)
-                     :accessor ,(getf field :accessor)))
+                     :accessor ,(getf field :name)))
               class-fields)))
 
+
+;;макрос для создания методов просмотра по списку параметров
 (defmacro new-classes.make-view-method (name class-fields)
   `(defmethod new-classes.make-fields ((object ,name))
      ,(cons
        `list
        (mapcar #'(lambda (field)
                    `(,(intern (string-upcase
-                               (format nil "new-classes.view-~a-field" (getf field :type))))
-                                      (,(getf field :name)  object)
-                                      ,(format nil "~a" (getf field :name))
-                                      ,(getf field :disabled)))
+                               (format nil "object-fields.~a-field-view" (getf field :type))))
+                      (,(getf field :name)  object)
+                      ,(format nil "~a" (getf field :name))
+                      ,(getf field :disabled)))
                class-fields))))
 
+
+;;макрос для создания методов редактирования
 (defmacro new-classes.make-edit-method (name class-fields)
   `(defmethod new-classes.edit-fields ((object ,name) post-data-plist)
      ,(cons
@@ -91,8 +34,8 @@
                    (when (not (getf field :disabled))
                      `(setf (,(getf field :name) object)
                             (,(intern (string-upcase
-                                       (format nil "new-classes.~a-field-get-data" (getf field :type))))
-                              (decode-uri (getf post-data-plist ,(intern (string-upcase (format nil "~a" (getf field :name))) :keyword)))))))
+                                       (format nil "object-fields.~a-field-get-data" (getf field :type))))
+                              (getf post-data-plist ,(intern (string-upcase (format nil "~a" (getf field :name))) :keyword))))))
                class-fields))))
 
 
@@ -342,5 +285,6 @@
 (defun new-classes.make-class-and-methods (name list-fields)
   (eval `(new-classes.make-class ,name ,list-fields))
   (eval `(new-classes.make-view-method ,name ,list-fields))
-  (eval `(new-classes.make-edit-method ,name ,list-fields)))
-
+  (eval `(new-classes.make-edit-method ,name ,list-fields))
+  (eval `(new-classes.make-unserialize-method ,name ,list-fields))
+  (eval `(new-classes.make-serialize-method ,name ,list-fields)))
